10682fc83880407ebf872852dcbfe22d7c81fd0a
Kush Vasaniya <vasaniyakush@gmail.com>
2023-09-05 17:47:17 +0530

    Update FactorialComponent.js

--- components/FactorialComponent.js
+++ components/FactorialComponent.js
@@ -1,15 +1,13 @@
-import { useMemo } from 'react';
+
 
 function factorial(n) {
   if (n <= 1) return 1;
   return n * factorial(n - 1);
 }
 
-export default function FactorialComponent({ num }) {
-  const result = useMemo(() => {
-    return factorial(num);
-  }, [num]);
+export default function FactorialComponent() {
 
-  return <div className='result'>The factorial of {num} is {result}</div>;
+
+  return <div className='result'>The factorial of {} is {}</div>;
 }
 
